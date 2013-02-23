106ea66a7ab9ecabb4d1f1614b4be14b1aec08b7
Alex Carruthers <alex@alexcarruthers.com>
2013-02-22 22:45:43 -0500

    initial commit

---
+++ minidc.py
@@ -0,0 +1,73 @@
+class minidc:
+    def __init__(self):
+        self.nums = []
+
+    #Takes a number and push it onto the stack so that
+    #it can be stored for future computation
+    def push_number(self, num):
+        #push number onto the stack
+        self.nums.append(num)
+
+    #pops the top two numbers off of the stack, adds them
+    #and pushes it back onto the stack
+    def add(self):
+        #pop the first number off the stack
+        num1 = self.nums.pop()
+        #pop the second number off the stack
+        num2 = self.nums.pop()
+        #add the numbers and pop the result back onto
+        #the stack
+        self.nums.append(num1 + num2)
+
+    #pops the top two numbers off of the stack, subtracts one
+    #from the other and pushes it back onto the stack
+    def subtract(self):
+        #pop the first number off the stack
+        num1 = self.nums.pop()
+        #pop the second number off the stack
+        num2 = self.nums.pop()
+        #add the numbers and pop the result back onto
+        #the stack
+        self.nums.append(num2 - num1)
+
+    #pops the top two numbers off of the stack and multiplies
+    #them together and pushes the result back onto the stack
+    def multiply(self):
+        #pop the first number off the stack
+        num1 = self.nums.pop()
+        #pop the second number off the stack
+        num2 = self.nums.pop()
+        #multiply the two numbers and push the result back
+        #onto the stack
+        self.nums.append(num1 * num2)
+
+    #pops two top numbers off the stack and divides the second
+    #by the first and pushes the result back onto the stack
+    def divide(self):
+        #pop the first number off the stack
+        num1 = self.nums.pop()
+        #pop the second number off the stack
+        num2 = self.nums.pop()
+        #divide num2 by num1 and push the result back onto the stack
+        self.nums.append(num2 / num1)
+
+    #peeks at the top number on the stack and prints it out
+    def command_p(self):
+        #pop the top number off the stack
+        topnum = self.nums.pop()
+        #push it back onto the stack
+        self.nums.append(topnum)
+        #print the number to stdout
+        print topnum
+
+    def command_n(self):
+        pass
+
+    def command_f(self):
+        pass
+
+    def execute_line(self):
+        pass
+
+    def run(self):
+        pass
---
+++ test_minidc.py
@@ -0,0 +1,103 @@
+import unittest
+import sys
+import StringIO
+from minidc import minidc
+
+
+class test_minidc(unittest.TestCase):
+    #test push_number with valid domain logic
+    def test_push_number_domain_correct(self):
+        #create a calculator
+        calc = minidc()
+        #push number 5 on the stack
+        calc.push_number(5)
+        #check to make sure the stack has length 1
+        self.assertEqual(len(calc.nums), 1)
+        #check to make sure the (only) number on the stack is 5
+        self.assertEqual(calc.nums.pop(), 5)
+
+    #test add with valid domain logic
+    def test_add_domain_correct(self):
+        #create a calculator
+        calc = minidc()
+        #push a 3 onto the stack
+        calc.push_number(3)
+        #push a 5 onto the stack
+        calc.push_number(5)
+        #add the two numbers together
+        calc.add()
+        #check to make sure the stack has length 1
+        self.assertEqual(len(calc.nums), 1)
+        #check to make sure the value on the stack is 8
+        self.assertEqual(calc.nums.pop(), 8)
+
+    #test subtract with valid domain logic
+    def test_subtract_domain_correct(self):
+        #create a calculator
+        calc = minidc()
+        #push a 5 onto the stack
+        calc.push_number(5)
+        #push a 3 onto the stack
+        calc.push_number(3)
+        #subtract the second number from the other
+        calc.subtract()
+        #check to make sure the stack has length 1
+        self.assertEqual(len(calc.nums), 1)
+        #check to make sure the value on the stack is 2
+        self.assertEqual(calc.nums.pop(), 2)
+
+    #test multiply with valid domain logic
+    def test_multiply_domain_correct(self):
+        #create a calculator
+        calc = minidc()
+        #push a 5 onto the stack
+        calc.push_number(5)
+        #push a 3 onto the stack
+        calc.push_number(3)
+        #multiply the numbers together
+        calc.multiply()
+        #check to make sure the stack has length 1
+        self.assertEqual(len(calc.nums), 1)
+        #check to make sure the value on the stack is 15
+        self.assertEqual(calc.nums.pop(), 15)
+
+    #test divide with valid domain logic
+    def test_divide_domain_correct(self):
+        #create a calculator
+        calc = minidc()
+        #push a 15 onto the stack
+        calc.push_number(15)
+        #push a 3 onto the stack
+        calc.push_number(3)
+        #divide the numbers
+        calc.divide()
+        #make sure the stack has length 1
+        self.assertEqual(len(calc.nums), 1)
+        #make sure value on stack is 5
+        self.assertEqual(calc.nums.pop(), 5)
+
+    #test the p command with valid domain logic
+    def test_command_p_domain_correct(self):
+        #create a calculator
+        calc = minidc()
+        #push a 4 onto the stack
+        calc.push_number(4)
+        #redirect system.out to something we can read
+        stdout_temp = sys.stdout
+        out = StringIO.StringIO()
+        sys.stdout = out
+
+        try:
+            #run the p command
+            calc.command_p()
+            #get the value printed
+            printed = out.getvalue()
+        finally:
+            #restore stdout
+            sys.stdout = stdout_temp
+        #assert that the stack length hasn't changed
+        self.assertEqual(len(calc.nums), 1)
+        #assert that the value printed is the values pushed
+        self.assertEqual(printed, "4\n")
+
+unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(test_minidc))
